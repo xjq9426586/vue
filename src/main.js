f5478064d2e2581c34628cfb051107d5f21d346c
xjq <931408673@qq.com>
2020-11-09 17:19:22 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import 'tsvue/lib/tstheme/index.css'
-import tsVue from 'tsvue'
 //import {Service} from './service.js'
 //Vue.prototype.$axios=Service
-Vue.use(tsVue)
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
